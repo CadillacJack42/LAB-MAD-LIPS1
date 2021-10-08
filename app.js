ca625a5e90e96571015c34e54eef74e6cb4f239e
James Keicher <jamesdkeicher@gmail.com>
2021-10-08 15:24:00 -0700

    started js

--- app.js
+++ app.js
@@ -9,14 +9,20 @@ const input6 = document.getElementById('input-6');
 const input7 = document.getElementById('input-7');
 const input8 = document.getElementById('input-8');
 const input9 = document.getElementById('input-9');
+const input10 = document.getElementById('input-10');
+const input11 = document.getElementById('input-11');
 const button = document.getElementById('submit-button');
 const paragraph = document.getElementsByClassName('mad-lib');
+
+const inputs = [input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11]
 
 
 // reference needed DOM elements
 
 button.addEventListener('click', () => {
-    
+    for (let i = 0; i < inputs.length; i++) {
+        document.getElementsByClassName('input-' + i).textValue = inputs[i].value;  
+    }
 });
 
 
