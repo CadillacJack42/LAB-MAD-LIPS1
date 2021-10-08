e57ef38d46647330e87eacfc7d2e71f92351abfb
James Keicher <jamesdkeicher@gmail.com>
2021-10-08 13:27:41 -0700

    First iteration of pomedora

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 // import functions
 
+const input1 = document.getElementById('input-1');
+const input2 = document.getElementById('input-2');
+const input3 = document.getElementById('input-3');
+const input4 = document.getElementById('input-4');
+const input5 = document.getElementById('input-5');
+const input6 = document.getElementById('input-6');
+const input7 = document.getElementById('input-7');
+const input8 = document.getElementById('input-8');
+const input9 = document.getElementById('input-9');
+const button = document.getElementById('submit-button');
+const paragraph = document.getElementsByClassName('mad-lib');
+
+
 // reference needed DOM elements
+
+button.addEventListener('click', () => {
+    
+});
 
 
 // set event listeners 
